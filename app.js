5f4c871ce7040163a240cfac7a8043a88e0f22b9
IHURY FERREIRA <82301009+ihuryferreira@users.noreply.github.com>
2022-02-28 23:54:36 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+//Criando Array
+var lista_coisas = Array()
+
+lista_coisas['frutas'] = Array()
+lista_coisas['pessoas'] = []
+//Adicionando dados dentro Array frutas
+lista_coisas['frutas'].push('Banana')
+lista_coisas['frutas'].push('Uva')
+
+
+console.log( lista_coisas)
